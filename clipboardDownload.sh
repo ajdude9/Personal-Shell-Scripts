6858c7323211e75f8f03f992d83996f97be7e9d8
ajdude9 <adamtheteeth@hotmail.com>
2025-01-30 19:24:29 +0000

    Tweaked notifications to not persist in tray.

--- clipboardDownload.sh
+++ clipboardDownload.sh
@@ -2,4 +2,4 @@
 xsel -ob 2> /dev/null > "Shell Scripts"/clipboardHolder.txt
 line=$(head -n 1 "Shell Scripts"/clipboardHolder.txt)
 yt-dlp -P Downloads -o "%(fulltitle)s" --remux-video mp4 --embed-metadata $line
-notify-send --app-name= "Clipboard Downloader" "Download complete."
+notify-send --hint=int:transient:1 --app-name= "Clipboard Downloader" "Download complete."
